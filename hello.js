1f918dc9a33780991164d9b506c7a30320889911
himanshugahalyan06 <gahlyanshab38@gmail.com>
2024-12-20 20:26:07 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,429 @@
+// // // // // // //let st1 = 'Himanshu'
+// // // // // // //let cr = 'kunal'
+// // // // // // //let celebrity = 'devanshu'
+// // // // // // //let pAeohi = 'Arohi'
+// // // // // // //let name = 'Shubham'
+
+
+// // // // // // //let array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// // // // // // //let arrayofstudents = ['kunal', 'shubham', 1, [], 'kartik', 'monu', 'shyam', true, 'true', ['bulla ki jana m kon']];
+// // // // // // //console.log(arrayofstudents.length, array1.length)
+// // // // // // //let collage = 'geeta';
+// // // // // // //let n1 = 20
+// // // // // // //let n2 = 30
+// // // // // // //let sum = n1 + n2
+// // // // // // //console.log(sum)
+
+// // // // // // //let word = 'hello';
+// // // // // // //let nword = console.log(word[4] + word[3] + word[2] + word[1] + word[0]);
+
+
+// // // // // // //let N1 = 'Sahil';
+// // // // // // //let reversed = N1.split('').reverse().join('');
+// // // // // // //console.log(reversed);
+
+// // // // // // //let N2 = 'Nitin'
+// // // // // // //console.log(N2[N2.length - 1] + N2[N2.length - 2] + N2[N2.length - 3] + N2[N2.length - 4] + N2[N2.length - 5]);
+
+// // // // // // //let animal = 'elephant';
+// // // // // // //let index = 0;
+// // // // // // //console.log('Index', index, animal[0]);
+// // // // // // //console.log('Index', index + 1, animal[1]);
+// // // // // // //console.log('Index', index + 2, animal[2]);
+// // // // // // //console.log('Index', index + 3, animal[3]);
+// // // // // // //console.log('Index', index + 4, animal[4]);
+// // // // // // //console.log('Index', index + 5, animal[5]);
+// // // // // // //console.log('Index', index + 6, animal[6]);
+// // // // // // //console.log('Index', index + 7, animal[7]);
+
+// // // // // // // to get data type of a variable in js 
+// // // // // // // log(typeof name of variable)
+
+// // // // // // //let a1 = 22;
+// // // // // // //console.log(typeof a1);
+// // // // // // //let str = 'hello all';
+// // // // // // //console.log(typeof str);
+// // // // // // //let b1 = '|22|' - 1;
+// // // // // // //console.log(typeof b1);
+
+// // // // // // //let num = 100;
+// // // // // // //if (5 > 2) {
+// // // // // // //  num = 'wooah';
+// // // // // // // console.log(num)
+// // // // // // //}
+// // // // // // //else {
+// // // // // // //  num = 111;
+// // // // // // //console.log(num);
+// // // // // // //}
+
+// // // // // // //let t = 'hello';
+// // // // // // //console.log(t.length)
+// // // // // // // create an array of numbers & string //
+// // // // // // //let students = [1, 20, 40, 'Himanshu', 50, true, 60, false, 'Sahil'];
+// // // // // // //console.log(students);
+
+// // // // // // //let arr = [1, 2, 'hello', '33'];
+// // // // // // //let len = (arr[2].length);
+// // // // // // //console.log(len.length);
+
+// // // // // // //let a = [1, [1, '11'], 1];
+// // // // // // //let chiken = a[1][1];
+// // // // // // //console.log(chiken);
+
+// // // // // // //let b = [[1, 2, 3, 4], ['hello', 3], [["shubham"]]];
+// // // // // // //console.log(b[1][0] + " " + 'sir' + " " + b[2][0][0]);
+
+// // // // // // //sum of all num in array
+// // // // // // //let l = [1, 2, [1, [1, 11]]];
+// // // // // // //let h = l[0] + l[1] + l[2][0] + l[2][1][0] + l[2][1][1];
+// // // // // // //console.log(h);
+
+// // // // // // //let e = ['k', 'c', 'a', 'b'];
+// // // // // // //console.log(e[3] + e[2] + e[1] + e[0]);
+
+// // // // // // // Object //
+// // // // // // // in this data type we can store all do,s of a vaiable //
+// // // // // // // key must be unique//
+// // // // // // //example let phone={'name':'iphone','model':'15 pro','color':'black','features':['call','video','cam']};
+
+
+// // // // // // // ouestion create a student object //
+// // // // // // //let H = [1, 2, 211, 2, 1];
+// // // // // // //let max = H[0];
+// // // // // // //for(let i=1;i<5;i++){
+// // // // // // // if(max<H[i])
+// // // // // // // max=H[i];
+// // // // // // //}
+// // // // // // //console.log(max)
+
+// // // // // // // Important  Methods of Array//
+
+// // // // // // // push method //this method add element in end of array//
+// // // // // // //let P=[1,2,7];
+// // // // // // //P.push(22);
+// // // // // // //P.push(1111);
+// // // // // // //P.push(0);
+// // // // // // //P.unshift(1000);
+// // // // // // //console.log(P)
+// // // // // // //to add element in starting of array// use arr.unshift()
+// // // // // // //DELETE last element from array// use arr.pop()
+// // // // // // //remove from start // use arr.shift()
+// // // // // // //reverse an array// use arr.reverse()
+
+// // // // // // //console.log (P.reverse())  
+// // // // // // //let L=[1,9,0]
+// // // // // // //L.shift()
+// // // // // // //L.pop()
+// // // // // // //console.log(L)
+// // // // // // //Add Element without unshift 
+// // // // // // //let R=[];
+// // // // // // //R.push(7)
+// // // // // // //R.push(1)
+// // // // // // //console.log(R)
+// // // // // // // TO make Function Write Keryword Function function Name (){code} //
+// // // // // // //function Hello(){
+// // // // // // // console.log('Hello World');
+// // // // // // //}
+// // // // // // //How to run a Function or call a function //
+// // // // // // // Name of Function()//
+// // // // // // //Hello()
+// // // // // // // Print sum of both the array using one for loop//
+// // // // // // //let arr1 = [1, 2];
+// // // // // // //let arr2 = [2, 1];
+// // // // // // //let sum = 0;
+// // // // // // //for (let i = 0; i < arr1.length; i++) {
+// // // // // // //   sum = sum + arr1[i] + arr2[i];
+// // // // // // //}
+// // // // // // //9console.log(sum);
+// // // // // // //let a=[1,2]
+// // // // // // //let b=[1,2,3,4]
+// // // // // // //let sum=0;
+// // // // // // //for(let i=0;i<b.length;i++){
+// // // // // // // if (i<a.length){
+// // // // // // //    sum=sum+a[i]+b[i]
+// // // // // // // } 
+// // // // // // // else{
+// // // // // // //     sum=sum+b[i]
+// // // // // // //  }
+// // // // // // //}
+// // // // // // //console.log(sum)
+
+// // // // // // // Create a function great that takes an arrgument and name if (name is present then log naaste name)
+// // // // // // //if not then log namaste boss//
+// // // // // // //function greet(a) {
+// // // // // //    // if (a) {
+// // // // // //        // console.log('Namaste' + " " + a)
+// // // // // //   //  }
+// // // // // //    // else {
+// // // // // //        // console.log(Namasteboss)
+// // // // // //    // }
+// // // // // // //}
+// // // // // // //greet("Himanshu");
+
+// // // // // // // create a function to fet something to it if num > 10 print('Big')
+// // // // // // //else (log small number ) 
+// // // // // // //let getsomething = (n) => {
+// // // // // //     //if (n > 10) {
+// // // // // //       //  console.log('Big Number');
+// // // // // //    // }
+// // // // // //     ///else {
+// // // // // //         //console.log('Small Number');
+// // // // // //   //  }
+// // // // // // //};
+// // // // // // //getsomething(16)
+// // // // // // //getsomething(6)
+// // // // // // // Question Fizz Buzz
+
+// // // // // // // Question 1
+// // // // // // // function printnumbers(){
+// // // // // // // for(let i=1;i<=10;i++){
+// // // // // // //     console.log(i+" ");
+// // // // // // // }
+// // // // // // // console.log();
+// // // // // // // }
+// // // // // // // printnumbers();
+
+// // // // // // // Question 2
+
+// // // // // // function printEvenNumbers() {
+// // // // // //     for (let i = 2; i <= 20; i += 2) {
+// // // // // //     console.log(i + " ");
+// // // // // //     }
+// // // // // //     console.log();
+// // // // // // }
+
+// // // // // // printEvenNumbers();
+// // // // // // function printOddNumbers() {
+// // // // // //     for (let i = 1; i <= 20; i += 2) {
+// // // // // //         console.log(i);
+
+// // // // // //     }
+// // // // // //     console.log();
+// // // // // // }
+
+// // // // // // printOddNumbers();
+// // // // // // function printSquares() {
+// // // // // //     for (let i = 1; i <= 5; i++) {
+// // // // // //         console.log(i * i);
+// // // // // //     }
+// // // // // // }
+
+
+// // // // // // printSquares();
+// // // // // function sumNumbers() {
+// // // // //     let sum = 0;
+// // // // //     for (let i = 1; i <= 100; i++) {
+// // // // //         sum += i;
+// // // // //     }
+// // // // //     console.log(sum);
+// // // // // }
+
+
+// // // // // sumNumbers();
+
+// // // // // function printArrayElements() {
+// // // // //      arr = [10, 20, 30, 40, 50];
+// // // // //     for (let i = 0; i < arr.length; i++) {
+// // // // //         console.log(arr[i]);
+// // // // //     }
+// // // // // }
+
+
+// // // // // printArrayElements();
+
+// // // // // function printElementsGreaterThan25() {
+// // // // //      arr = [10, 20, 30, 40, 50];
+// // // // //     for (let i = 0; i < arr.length; i++) {
+// // // // //         if (arr[i] > 25) {
+// // // // //             console.log(arr[i]);
+// // // // //         }
+// // // // //     }
+// // // // // }
+// // // // // printElementsGreaterThan25();
+
+// // // // function sumArrayElements() {
+// // // //      arr = [10, 20, 30, 40, 50];
+// // // //     let sum = 0;
+// // // //     for (let i = 0; i < arr.length; i++) {
+// // // //         sum += arr[i];
+// // // //     }
+// // // //     console.log(sum);
+// // // // }
+// // // // sumArrayElements();
+
+// // // // function factorial(num) {
+// // // //     let result = 1;
+// // // //     for (let i = 1; i <= num; i++) {
+// // // //         result *= i;
+// // // //     }
+// // // //     console.log(result);
+// // // // }
+// // // // factorial(5);
+
+// // // // function printReverse() {
+// // // //     for (let i = 10; i >= 1; i--) {
+// // // //         console.log(i);
+// // // //     }
+// // // // }
+
+// // // // printReverse();
+
+// // // // function multiplicationTable() {
+// // // //     for (let i = 1; i <= 10; i++) {
+// // // //         console.log(7 * i);
+// // // //     }
+// // // // }
+
+// // // // multiplicationTable();
+
+
+// // // // function printNumbers() {
+// // // //     let i = 1;
+// // // //     while (i <= 10) {
+// // // //         console.log(i);
+// // // //         i++;
+// // // //     }
+// // // // }
+// // // // printNumbers();
+
+
+// // // // function sumNumbers() {
+// // // //     let i = 1;
+// // // //     let sum = 0;
+// // // //     while (i <= 50) {
+// // // //         sum += i;
+// // // //         i++;
+// // // //     }
+// // // //     console.log(sum);
+// // // // }
+// // // // sumNumbers();
+
+// // // // function printOddNumbers() {
+// // // //     let i = 1;
+// // // //     while (i <= 30) {
+// // // //         console.log(i);
+// // // //         i += 2;  
+// // // //     }
+// // // // }
+// // // // printOddNumbers();
+
+
+// // // // function countdown() {
+// // // //     let i = 10;
+// // // //     while (i >= 0) {
+// // // //         console.log(i);
+// // // //         i--;
+// // // //     }
+// // // // }
+// // // // countdown();
+
+// // // // function printHelloWorld() {
+// // // //     let i = 0;
+// // // //     do {
+// // // //         console.log("Hello, World!");
+// // // //         i++;
+// // // //     } while (i < 5);
+// // // // }
+// // // // printHelloWorld();
+
+// // // // function printArray() {
+// // // //      arr = ['apple', 'banana', 'cherry'];
+// // // //     arr.forEach(element => {
+// // // //         console.log(element);
+// // // //     });
+// // // // }
+
+// // // // printArray();
+
+// // // // function findLargestNumber() {
+// // // //      arr = [3, 1, 5, 9, 2];
+// // // //     let largest = arr[0];
+// // // //     for (let i = 1; i < arr.length; i++) {
+// // // //         if (arr[i] > largest) {
+// // // //             largest = arr[i];
+// // // //         }
+// // // //     }
+// // // //     console.log(largest);
+// // // // }
+// // // // findLargestNumber();
+
+// // // // function countFives() {
+// // // //      arr = [1, 5, 3, 5, 4, 5];
+// // // //     let count = 0;
+// // // //     for (let i = 0; i < arr.length; i++) {
+// // // //         if (arr[i] === 5) {
+// // // //             count++;
+// // // //         }
+// // // //     }
+// // // //     console.log(count);
+// // // // }
+// // // // countFives();
+
+
+// // // // function reverseString() {
+// // // //     let str = 'hello';
+// // // //     let reversed = '';
+// // // //     for (let i = str.length - 1; i >= 0; i--) {
+// // // //         reversed += str[i];
+// // // //     }
+// // // //     console.log(reversed);
+// // // // }
+// // // // reverseString();
+
+
+// // // //let arr = [0, 1, 2, 0, -7, 5, 6]
+
+
+// // //   //  for (let i = 0; i < 7; i++) {
+// // //        // if (arr[i] != 0) {
+// // //           //  arr.push(arr[i] * arr[i]);
+
+
+// // // //console.log(arr[i]*arr[i])}
+// // //        // }
+// // //       let o={a:21};
+// // //        o.a=25;
+// // //        o.b=7;
+// // //        o["h"]=60
+// // //        console.log(o);
+// // //        // another way to access object //
+// // //        let c={'hello all': 21}
+// // //        c['hello all']=25
+// // //        c['i']=50
+// // // console.log(c)
+
+// // // convert array into object //
+// // let arr=[1,2,3,91,62]
+// // let H={};
+// // for(let i =0; i<arr.length;i++){
+//     // H[arr[i]]='ok';
+//     // }
+//     // console.log(H)   
+//     // // // count frequency//
+//     // // convert array into onject and count frequency of a number 
+    
+//     let para = document.getElementById('Hi')
+//     console.log(para)
+//     console.dir(para)  // to print onject of element (to make DOM of element )//
+//     para.textContent = 'MY Name is Himanshu Gahalyan ' // change text of HTML tag//
+//     // change text color of html tag using js//
+//     para.style.color = 'Orange'
+//     // create green circle using DOM manipulation //
+//     let sahil = document.getElementById('t1')
+//     console.dir(sahil)
+    
+//     /// create p tag using java script and add it into html tag //
+    
+//     let div=document.getElementById('d')
+//     let p=document.createElement('p')
+//     p.textContent='Himanshu Gahalyan '
+//     div.appendChild(p)
+    
+    
+//     let clickme =()=>{
+//     let sd= document.getElementsByTagName('img')[0];
+//     sd.src = "cradle of military.jpg";
+// }
+
+// closer and caring have to learn //
+
